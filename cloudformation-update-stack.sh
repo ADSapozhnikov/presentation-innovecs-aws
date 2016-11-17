22a204c4d355dd41a2dfb0f18b86be64089fdb01
Alexey Konokhov <a.konokhov@gmail.com>
2016-11-17 15:07:28 +0200

    1

--- cloudformation-update-stack.sh
+++ cloudformation-update-stack.sh
@@ -2,7 +2,7 @@
 
 aws --profile demo cloudformation update-stack \
     --stack-name Demo \
-    --template-body file://.//$1 \
+    --template-body file://.//cloudformation-template.yaml \
     --parameters ParameterKey=KeyName,ParameterValue=aws-personal-demo-us-east-1 \
     --capabilities CAPABILITY_IAM
 
